37d5b530cf9511a31e8b2f3063ae3cfdc85dfc3d
Young Jorisky <jojoyoung3@gmail.com>
2026-09-23 19:36:32 +0000

    Modified hardcoded localhost and updated gitignore

--- src/Pages/real-page/Appointment.tsx
+++ src/Pages/real-page/Appointment.tsx
@@ -28,7 +28,7 @@ export default function Appointment() {
     setStatus({ loading: true, success: null, error: null });
 
     try {
-      const response = await fetch("http://localhost:5000/api/contact", {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/api/contact`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
